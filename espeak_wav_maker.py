ceac389bd621b438babb686f9244f397bb24b377
Alex Childers <42629593+abchilders@users.noreply.github.com>
2020-08-15 18:44:32 -0700

    Use argparse to make TTS script user-friendly(ish)

--- espeak_wav_maker.py
+++ espeak_wav_maker.py
@@ -1,31 +1,35 @@
 import os
 import sys
+import argparse
 from datetime import datetime
 
 def say(text):
     os.popen('espeak "' + text + '" --stdout | aplay 2>/dev/null')
 
 def create_wav(text, file_name):
-    os.popen('espeak "' + text + '" --stdout > ' + file_name + '.wav')
-
+    os.popen('espeak "' + text + '" --stdout > ' + file_name)
+    
 def main():
-    # remove file name from arguments list
-    print sys.argv
-    text = sys.argv
-    text.pop(0)
+    # set up argument parser
+    parser = argparse.ArgumentParser(description='Uses TTS to convert STDIN to a .wav file.')
+    parser.add_argument('-f', '--file', nargs='?', type=str, help='The file name to output audio to (default: current datetime)')
+    parser.add_argument('text', nargs='+', type=str, help='The text to be converted to speech')
+    args = parser.parse_args()
+    
+    # if output file not specified, get current date and time for naming .wav output file
+    if(args.file):
+        filename = args.file
+    else:
+        now = datetime.now()
+        filename = now.strftime("%d-%m-%Y_%H:%M:%S:%f") + '.wav'
+    print('creating ' + filename)
 
-    # now reformat as whole string
-    text = str(' '.join(sys.argv))
-
-    # get current date and time for naming .wav output file
-    now = datetime.now()
-    dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
-    print(dt_string)
-
-    # make espeak wav of given text and read back out loud
-    print(text)
-    create_wav(text, dt_string)
+    # make espeak wav of given text
+    text = args.text
+    text = str(' '.join(text))
+    create_wav(text, filename)
+    #read back file out loud 
     #say(text)
-    #os.popen('aplay ' + dt_string + '.wav')
+    #os.popen('aplay ' + filename)
 
 main()
